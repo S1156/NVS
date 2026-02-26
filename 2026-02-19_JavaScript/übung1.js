d2ea9a115c6d67e584f7b4bef2f1870ae670d68b
Stefan Pavkovic <s23268073@gmail.com>
2026-02-26 19:47:59 +0100

    deno

--- 2026-02-19_JavaScript/übung1.js
+++ 2026-02-19_JavaScript/übung1.js
@@ -1,4 +1,9 @@
 const numberCount = 5;
 const min = -2;
 const max = 100;
-
+const range = max-min;
+const array = [];
+for (let i = 0; i < numberCount; i++){
+    array.push(Math.random() * range);
+}
+console.log(array);
